22247647156e81f25048e57dcd328dbea1f08b47
NikitaHessRTU <hess3@inbox.lv>
2024-10-16 11:18:28 +0300

    truncateText -> tailwind styling

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -17,29 +17,22 @@ interface TaskListProps {
 
 export const TaskList: React.FC<TaskListProps> = ({ tasks, removeTask, changeStatus }) => {
 
-  const truncateDescription = (description: string, length: number) => {
-    if (description.length > length) {
-      return `${description.substring(0, length)}...`;
-    }
-    return description;
-  };
-
   return (
     <div className="flex flex-col gap-4">
       {tasks.length ? (
         tasks.map((task: Task) => (
           <div key={task.id} className="flex justify-between items-center border py-2 px-4 rounded-lg w-full">
             <div className="flex items-center flex-grow w-2/3">
-              <div className="flex-grow">
+              <div className="flex-grow max-w-full">
                 <p className="font-semibold text-base break-all">{task.name}</p>
                 <Tooltip title={task.description} placement="top">
-                  <p className="text-sm break-words text-gray-500 break-normal all">
-                    {truncateDescription(task.description, 50)}
+                  <p className="text-sm break-words text-gray-500 truncate">
+                    {task.description}
                   </p>
                 </Tooltip>
               </div>
             </div>
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2 w-1/3 justify-end">
                 <select 
                 className="border rounded-md px-2 py-1 text-sm" 
                 value={task.status} 
